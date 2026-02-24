67af63d60dfc271009fce5b31a08a9c92737c64b
Omar Ahmed <251omarahmed@gmail.com>
2026-02-24 08:43:27 +0200

    Update localize_ button.dart

--- lib/reusable_widget/localize_ button.dart
+++ lib/reusable_widget/localize_ button.dart
@@ -3,37 +3,56 @@ import 'package:movie_app/utils/app_assets.dart';
 import 'package:movie_app/utils/app_color.dart';
 
 class LocalizeButton extends StatefulWidget {
-   LocalizeButton({super.key});
 
+  LocalizeButton( {super.key,});
   @override
   State<LocalizeButton> createState() => _LocalizeButtonState();
 }
-
 class _LocalizeButtonState extends State<LocalizeButton> {
-   List<bool> isSelected=[false,false];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ToggleButtons(isSelected: isSelected,onPressed: (index) {
-        for(int i = 0; i < isSelected.length; i++){
-          isSelected[i] = i == index;
-          setState(() {
-          });
-        }
-      },borderWidth:3,
-        borderColor: AppColor.yellowColor ,
-        borderRadius: BorderRadius.circular(30),
-        selectedColor:AppColor.yellowColor,
-        fillColor:AppColor.yellowColor ,
-        children:[
-          CircleAvatar(
-            radius: 15,
-            child: Image.asset(AppAssets.languageEN),),
-          CircleAvatar(
-            radius: 15,
-            child: Image.asset(AppAssets.languageAR),),
-        ],),
+     bool isEnSelected = languageProvider.appLanguage == "en";
+    bool isArSelected = languageProvider.appLanguage == "ar";
+    var width=MediaQuery.of(context).size. width;
+    var height=MediaQuery.of(context).size.height;
+    return Container(width: width*.32,height: height*.058,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadiusGeometry.circular(30),
+          border: Border.all(width: 3,color: AppColor.yellowColor,) ),
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Row(
+           spacing:width*.1,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(onTap: () {
+                
+              },
+                child: Container(
+                    decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border:isEnSelected? Border.all(width: 4,color: AppColor.yellowColor):null,
+                ), child: CircleAvatar(
+                    radius: 20,
+                    child: Image.asset(AppAssets.languageEN) ),
+                ),
+              ),
+              InkWell(onTap: () {
+         
+              },
+                child: Container(
+                  decoration: BoxDecoration(
+                   shape: BoxShape.circle,
+                    border:isArSelected? Border.all(width: 4,color: AppColor.yellowColor):null,
+                  ),child: CircleAvatar(
+                    radius: 18,
+                    child: Image.asset(AppAssets.languageAR),),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
